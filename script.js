045e4344d417e8595ca76115eb6f27f090b66a6f
maurice <mauricemiema90@gmail.com>
2024-06-22 01:05:15 +0200

    ajout d'un fichier js et les annimation via scrollreveal

---
+++ script.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.debut',{
+    delay : 1000,
+    durection : 600,
+    origin : 'top',
+    distance : '50px',
+    // scale : 2,
+    interval: 600,
+    easing: 'ease-in-out' 
+})
+
+ScrollReveal().reveal('.simple',{
+    delay : 3000,
+    durection : 600,
+    origin : 'top',
+    distance : '50px',
+    easing: 'ease-in-out' 
+})
+
+ScrollReveal().reveal('.btn',{
+    delay : 4000,
+    durection : 600,
+    origin : 'top',
+    distance : '50px',
+    scale : 5,
+    interval: 600,
+    easing: 'ease-in-out' 
+})
+
