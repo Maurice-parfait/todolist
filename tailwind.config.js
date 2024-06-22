2ebf2636ac2a515e9c75153a5bb5eb3d4a6cc2bb
maurice <mauricemiema90@gmail.com>
2024-06-23 00:45:05 +0200

    partie de la to do list en couurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./script.js"],
+  content: ["./index.html", "./script.js", "./todolist.html"],
   theme: {
     extend: {
       fontFamily : {
