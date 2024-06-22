29e1fcb95397712c363ffccb2b71dada67d5db77
maurice <mauricemiema90@gmail.com>
2024-06-23 01:30:58 +0200

    to do lidt en cours

--- script.js
+++ script.js
@@ -26,3 +26,76 @@ ScrollReveal().reveal('.btn',{
     easing: 'ease-in-out' 
 })
 
+
+
+
+
+
+
+
+
+
+// la deuxieme page 
+
+
+let liste = document.getElementById("tache");
+
+function ajouterunetache() {
+    let ajouter = document.getElementById("ajout_tache");
+    let tasktext = ajouter.value; 
+
+    if (tasktext === "") {
+        return;
+    }
+
+    let li = document.createElement(li);
+    li.innerHTML = tasktext;
+
+    let modifierbouton = document.createElement("button");
+
+    modifierbouton.innerHTML = "<ion-icon name="pencil-outline"></ion-icon>"
+
+    modifierbouton.onclick = function() {
+        modifierlist(li)
+    }
+
+    // pour la suppression 
+
+    let supprimerbouton = document.createElement("button");
+
+    supprimerbouton.innerHTML = "<ion-icon name="trash-outline"></ion-icon>"
+
+    supprimerbouton.onclick = function() {
+        supprimerlist(li)
+    }
+
+
+    li.appendChild(modifierbouton);
+    li.appendChild(supprimerbouton);
+
+
+    liste.appendChild(li);
+
+    ajouter.value = "";
+
+
+}
+
+
+function modifierlist(task) {
+    let tasktextElement = task.firstChid;
+    let tasktext = tasktextElement.textContent;
+
+
+    let newtasktext = prompt("Modifier la tache:", tasktext)
+
+    if (newtasktext === null || newtasktext === "") {
+        return;
+    }
+
+    tasktextElement.textContent = newtasktext;
+
+}
+
+
+
