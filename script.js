8b3dd7c32684fffc0e7e868078509c5a16eb2029
maurice <mauricemiema90@gmail.com>
2024-06-23 02:20:14 +0200

    je suis limiter au niveau de la to do list et je doit arrenger la liste ui s'affiche (design)

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-ScrollReveal().reveal('.debut',{
-    delay : 1000,
-    durection : 600,
-    origin : 'top',
-    distance : '50px',
-    // scale : 2,
+ScrollReveal().reveal('.debut', {
+    delay: 1000,
+    duration: 600, // correction de 'durection' à 'duration'
+    origin: 'top',
+    distance: '50px',
+    // scale: 2,
     interval: 600,
-    easing: 'ease-in-out' 
-})
+    easing: 'ease-in-out'
+});
 
-ScrollReveal().reveal('.simple',{
-    delay : 3000,
-    durection : 600,
-    origin : 'top',
-    distance : '50px',
-    easing: 'ease-in-out' 
-})
+ScrollReveal().reveal('.simple', {
+    delay: 3000,
+    duration: 600, // correction de 'durection' à 'duration'
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out'
+});
 
-ScrollReveal().reveal('.btn',{
-    delay : 4000,
-    durection : 600,
-    origin : 'top',
-    distance : '50px',
-    scale : 5,
+ScrollReveal().reveal('.btn', {
+    delay: 4000,
+    duration: 600, // correction de 'durection' à 'duration'
+    origin: 'top',
+    distance: '50px',
+    scale: 5,
     interval: 600,
-    easing: 'ease-in-out' 
-})
-
+    easing: 'ease-in-out'
+});
 
 
 
@@ -37,65 +36,61 @@ ScrollReveal().reveal('.btn',{
 
 // la deuxieme page 
 
-
 let liste = document.getElementById("tache");
+const btnajt = document.getElementById("btnajt");
 
-function ajouterunetache() {
+btnajt.addEventListener("click", function () {
     let ajouter = document.getElementById("ajout_tache");
-    let tasktext = ajouter.value; 
+    let tasktext = ajouter.value;
 
     if (tasktext === "") {
         return;
     }
 
-    let li = document.createElement(li);
-    li.innerHTML = tasktext;
+    let li = document.createElement("li");
+    li.textContent = tasktext;
+
+    li.classList.add('w-full','bg-red-400'); // Ajouter les classes Tailwind
 
     let modifierbouton = document.createElement("button");
-
-    modifierbouton.innerHTML = "<ion-icon name="pencil-outline"></ion-icon>"
+    modifierbouton.innerHTML = '<ion-icon name="pencil-outline"></ion-icon>';
+    
+    modifierbouton.classList.add('text-green-500');
 
     modifierbouton.onclick = function() {
-        modifierlist(li)
+        modifierlist(li); 
     }
-
-    // pour la suppression 
 
     let supprimerbouton = document.createElement("button");
+    supprimerbouton.innerHTML = '<ion-icon name="trash-outline"></ion-icon>';
 
-    supprimerbouton.innerHTML = "<ion-icon name="trash-outline"></ion-icon>"
-
+    supprimerbouton.classList.add('text-green-500'); 
+    
     supprimerbouton.onclick = function() {
-        supprimerlist(li)
+        supprimerlist(li);
     }
-
 
     li.appendChild(modifierbouton);
     li.appendChild(supprimerbouton);
 
-
     liste.appendChild(li);
-
     ajouter.value = "";
-
-
-}
-
+});
 
 function modifierlist(task) {
-    let tasktextElement = task.firstChid;
+    let tasktextElement = task.firstChild; 
     let tasktext = tasktextElement.textContent;
 
-
-    let newtasktext = prompt("Modifier la tache:", tasktext)
+    let newtasktext = prompt("Modifier la tache:", tasktext);
 
     if (newtasktext === null || newtasktext === "") {
         return;
     }
 
     tasktextElement.textContent = newtasktext;
-
 }
 
-
+function supprimerlist(task) {
+    liste.removeChild(task); 
+}
 
