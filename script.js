255a96bb756179c5bfad95aac3287456c447cf72
maurice <mauricemiema90@gmail.com>
2024-06-23 13:20:38 +0200

    todolist finsh

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ScrollReveal().reveal('.debut', {
     delay: 1000,
-    duration: 600, // correction de 'durection' à 'duration'
+    duration: 600,
     origin: 'top',
     distance: '50px',
     // scale: 2,
@@ -10,7 +10,7 @@ ScrollReveal().reveal('.debut', {
 
 ScrollReveal().reveal('.simple', {
     delay: 3000,
-    duration: 600, // correction de 'durection' à 'duration'
+    duration: 600, 
     origin: 'top',
     distance: '50px',
     easing: 'ease-in-out'
@@ -18,7 +18,7 @@ ScrollReveal().reveal('.simple', {
 
 ScrollReveal().reveal('.btn', {
     delay: 4000,
-    duration: 600, // correction de 'durection' à 'duration'
+    duration: 600, 
     origin: 'top',
     distance: '50px',
     scale: 5,
@@ -50,12 +50,12 @@ btnajt.addEventListener("click", function () {
     let li = document.createElement("li");
     li.textContent = tasktext;
 
-    li.classList.add('w-full','bg-red-400'); // Ajouter les classes Tailwind
+    li.classList.add('w-full', 'mb-2', 'p-2', 'rounded-xl', 'shadow-md',); // Ajouter les classes Tailwind
 
     let modifierbouton = document.createElement("button");
     modifierbouton.innerHTML = '<ion-icon name="pencil-outline"></ion-icon>';
     
-    modifierbouton.classList.add('text-green-500');
+    modifierbouton.classList.add('text-slate-50', 'px-2', 'py-1', 'border', 'ml-8', 'rounded-full', 'bg-green-600', 'text-2xl');
 
     modifierbouton.onclick = function() {
         modifierlist(li); 
@@ -64,7 +64,7 @@ btnajt.addEventListener("click", function () {
     let supprimerbouton = document.createElement("button");
     supprimerbouton.innerHTML = '<ion-icon name="trash-outline"></ion-icon>';
 
-    supprimerbouton.classList.add('text-green-500'); 
+    supprimerbouton.classList.add('text-slate-50', 'px-2', 'py-1', 'border', 'ml-10', 'rounded-full', 'bg-red-600', 'text-2xl'); 
     
     supprimerbouton.onclick = function() {
         supprimerlist(li);
